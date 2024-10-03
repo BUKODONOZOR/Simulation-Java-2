69e419b8f953a4674bd0575a7b18a1a7df955b48
BUKODONOZOR <jafetdavidibarguenlozano@pascualbravo.edu.co>
2024-10-03 16:52:10 -0500

    feat: initialization java of project

---
+++ src/main/java/com/Main/Simulation/SimulationApplication.java
@@ -0,0 +1,13 @@
+package com.Main.Simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationApplication.class, args);
+	}
+
+}
